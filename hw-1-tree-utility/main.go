66200e1bf2b6da1a564210fd651e16ac7abec217
Bronn1 <Bronn1>
2023-03-11 16:22:54 +0300

    hw1: completed

--- hw-1-tree-utility/main.go
+++ hw-1-tree-utility/main.go
@@ -2,11 +2,109 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
-	"strings"
 )
+
+type ByteWriter interface {
+	Write(b []byte) (n int, err error)
+}
+
+type TreeNode struct {
+	File   os.DirEntry
+	Childs []TreeNode
+}
+
+func (node *TreeNode) Empty() bool {
+	return node.File == nil
+}
+
+func (node *TreeNode) FormattedName() string {
+	if node.Empty() {
+		return ""
+	}
+
+	if node.File.IsDir() {
+		return node.File.Name()
+	} else {
+		return fmt.Sprintf("%s (%s)", node.File.Name(), node.Size())
+	}
+}
+
+func (node *TreeNode) Size() string {
+	if node.Empty() {
+		return ""
+	}
+
+	fileInfo, _ := node.File.Info()
+	if fileInfo.Size() > 0 {
+		return fmt.Sprintf("%db", fileInfo.Size())
+	} else {
+		return "empty"
+	}
+}
+
+func getDirPanics(path string) []os.DirEntry {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return files
+}
+
+func makeDirTree(path string, isPrintFiles bool) []TreeNode {
+	files := getDirPanics(path)
+	var tree []TreeNode
+	for _, file := range files {
+		if !file.IsDir() && !isPrintFiles {
+			continue
+		}
+
+		node := TreeNode{file, nil}
+		if file.IsDir() {
+			node.Childs = makeDirTree(filepath.Join(path, file.Name()), isPrintFiles)
+		}
+		tree = append(tree, node)
+	}
+
+	return tree
+}
+
+func printDirTree(out ByteWriter, tree []TreeNode, pathPrefix string) {
+	if tree == nil {
+		return
+	}
+	const (
+		levelSymbol        = "│\t"
+		dirOfOneElemSymbol = "\t"
+		leafSymbol         = "├───"
+		lastLeafSymbol     = "└───"
+	)
+
+	for i := 0; i < len(tree); i++ {
+		printLeafSymbol := leafSymbol
+		lvlSymbol := levelSymbol
+		if i == len(tree)-1 {
+			printLeafSymbol = lastLeafSymbol
+			lvlSymbol = dirOfOneElemSymbol
+		}
+
+		fmt.Fprint(out, pathPrefix, printLeafSymbol, tree[i].FormattedName(), "\n")
+		//out.Write([]byte(pathPrefix + printLeafSymbol + tree[i].FormattedName() + "\n"))
+		printDirTree(out, tree[i].Childs, pathPrefix+lvlSymbol)
+	}
+}
+
+func dirTree(treeResults ByteWriter, path string, isPrintFiles bool) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Fprint(treeResults, "Error occured: ", err)
+		}
+	}()
+	tree := makeDirTree(path, isPrintFiles)
+	printDirTree(treeResults, tree, "")
+}
 
 func main() {
 	out := os.Stdout
@@ -15,8 +113,6 @@ func main() {
 	}
 	path := os.Args[1]
 	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
-	err := dirTree(out, path, printFiles)
-	if err != nil {
-		panic(err.Error())
-	}
+	dirTree(out, path, printFiles)
+	fmt.Printf("%v", out)
 }
--- hw-1-tree-utility/main_test.go
+++ hw-1-tree-utility/main_test.go
@@ -38,11 +38,9 @@ const testFullResult = `├───project
 
 func TestTreeFull(t *testing.T) {
 	out := new(bytes.Buffer)
-	err := dirTree(out, "testdata", true)
-	if err != nil {
-		t.Errorf("test for OK Failed - error")
-	}
+	dirTree(out, "testdata", true)
 	result := out.String()
+
 	if result != testFullResult {
 		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, testFullResult)
 	}
@@ -64,12 +62,129 @@ const testDirResult = `├───project
 
 func TestTreeDir(t *testing.T) {
 	out := new(bytes.Buffer)
-	err := dirTree(out, "testdata", false)
-	if err != nil {
-		t.Errorf("test for OK Failed - error")
-	}
+	dirTree(out, "testdata", false)
 	result := out.String()
 	if result != testDirResult {
 		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, testDirResult)
+	}
+}
+
+const testdata2DirResult = `├───1_test.go (empty)
+├───a_test.go (empty)
+├───dir1
+│	└───dir2
+│		└───dir3
+│			└───dir4
+│				└───r.txt (2b)
+├───empty_dir
+└───testdata
+	├───project
+	│	├───file.txt (19b)
+	│	└───gopher.png (70372b)
+	├───static
+	│	├───a_lorem
+	│	│	├───dolor.txt (empty)
+	│	│	├───gopher.png (70372b)
+	│	│	└───ipsum
+	│	│		└───gopher.png (70372b)
+	│	├───css
+	│	│	└───body.css (28b)
+	│	├───empty.txt (empty)
+	│	├───html
+	│	│	└───index.html (57b)
+	│	├───js
+	│	│	└───site.js (10b)
+	│	└───z_lorem
+	│		├───dolor.txt (empty)
+	│		├───gopher.png (70372b)
+	│		└───ipsum
+	│			└───gopher.png (70372b)
+	├───zline
+	│	├───empty.txt (empty)
+	│	└───lorem
+	│		├───dolor.txt (empty)
+	│		├───gopher.png (70372b)
+	│		└───ipsum
+	│			└───gopher.png (70372b)
+	└───zzfile.txt (empty)
+`
+
+func TestData2Fulldir(t *testing.T) {
+	out := new(bytes.Buffer)
+	dirTree(out, "testdata2", true)
+	result := out.String()
+	if result != testdata2DirResult {
+		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, testdata2DirResult)
+	}
+}
+
+const testEmptyDirResult = ``
+
+func TestTreeEmptyDir(t *testing.T) {
+	out := new(bytes.Buffer)
+	dirTree(out, "testdata2/empty_dir", true)
+	result := out.String()
+	if result != testEmptyDirResult {
+		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, testEmptyDirResult)
+	}
+}
+
+func TestTreeEmptyDirNoFiles(t *testing.T) {
+	out := new(bytes.Buffer)
+	dirTree(out, "testdata/static/css", false) // folder contains one file, shouldn't be printed
+	result := out.String()
+
+	if result != testEmptyDirResult {
+		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, testEmptyDirResult)
+	}
+}
+
+const testDirOneFileResult = `└───site.js (10b)
+`
+
+func TestDirWithOneFile(t *testing.T) {
+	out := new(bytes.Buffer)
+	dirTree(out, "testdata/static/js", true)
+	result := out.String()
+
+	if result != testDirOneFileResult {
+		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, testDirOneFileResult)
+	}
+}
+
+const testDirOneDirResult = `└───ipsum
+`
+
+func TestDirWithOneDir(t *testing.T) {
+	out := new(bytes.Buffer)
+	dirTree(out, "testdata/static/a_lorem", false)
+	result := out.String()
+
+	if result != testDirOneDirResult {
+		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, testDirOneDirResult)
+	}
+}
+
+const invalidDirResult = `Error occured: open invaliddir: no such file or directory`
+
+func TestPanicOnInvalidDir(t *testing.T) {
+	out := new(bytes.Buffer)
+	dirTree(out, "invaliddir", false)
+	result := out.String()
+
+	if result != invalidDirResult {
+		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, invalidDirResult)
+	}
+}
+
+const notDirErrorResult = `Error occured: readdirent testdata/static/js/site.js: not a directory`
+
+func TestPanicOnFile(t *testing.T) {
+	out := new(bytes.Buffer)
+	dirTree(out, "testdata/static/js/site.js", true)
+	result := out.String()
+
+	if result != notDirErrorResult {
+		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, notDirErrorResult)
 	}
 }
